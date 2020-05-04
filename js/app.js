cbdf529fa5822d054e22089078fbb3e267a07ce0
rickpaige <rickypaige999@outlook.com>
2020-05-04 09:53:17 -0500

    UI draft

---
+++ js/app.js
@@ -0,0 +1,33 @@
+// // https://covidtracking.com/api/states
+
+// let request = new XMLHttpRequest();
+
+
+
+// request.onload = function() {
+//     let result = JSON.parse(this.responseText)
+    
+// }
+
+
+
+
+
+
+
+
+
+
+// request.open("GET", "https://covidtracking.com/api/states")
+// request.send()
+
+function myFunction() {
+    var x = document.getElementById("menuNav");
+        if (x.className === "menu") {
+            x.className += " responsive";
+        } else {
+            x.className = "menu";
+    }
+    
+  }
+
